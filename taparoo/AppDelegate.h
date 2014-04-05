7368f300bf2aba406e71dd4b47dbbdc6abfd26e9
Tony Albor <talbo001@ucr.edu>
2014-04-05 16:56:35 -0700

    intitial commit

---
+++ taparoo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  taparoo
+//
+//  Created by Tony Albor on 4/5/14.
+//  Copyright (c) 2014 tonyalbor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
