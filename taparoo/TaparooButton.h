cba4d738c654be014752fc84ec23f54a59496d33
Tony Albor <talbo001@ucr.edu>
2014-04-05 17:35:22 -0700

    commit for pull

---
+++ taparoo/TaparooButton.h
@@ -0,0 +1,13 @@
+//
+//  TaparooButton.h
+//  taparoo
+//
+//  Created by Tony Albor on 4/5/14.
+//  Copyright (c) 2014 tonyalbor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TaparooButton : UIButton
+
+@end
