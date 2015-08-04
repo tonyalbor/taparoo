c0d304375560ad56f8f26b15d81b0a7b1961a5cc
Tony Albor <tony.albor@yahoo.com>
2015-08-04 13:57:15 -0700

    Initial Commit

---
+++ Taparoo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Taparoo
+//
+//  Created by Tony Albor on 8/4/15.
+//  Copyright © 2015 Tony Albor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
