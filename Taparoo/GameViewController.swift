c3a385a1d3fa60ea04e4489b2726c82214ccb899
Tony Albor <tonyalbor92@gmail.com>
2015-11-03 23:43:32 -0800

    pause logic in game vc

--- Taparoo/GameViewController.swift
+++ Taparoo/GameViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class GameViewController: UIViewController {
     
+    @IBOutlet weak var timeLeftLabel: UILabel!
     var game: Game?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if let game = game {
+            timeLeftLabel.text = ":\(game.timeLeft)"
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,6 +38,20 @@ class GameViewController: UIViewController {
         
         if let game = game {
             game.restart()
+        }
+    }
+    
+    @IBAction func didPressPauseButton(sender: TaparooButton) {
+        
+        if let game = game {
+            
+            if game.isPaused && game.resume() {
+                // game is now resumed
+                sender.setTitle("Pause", forState: .Normal)
+            } else if !game.isPaused && game.pause() {
+                // game is now paused
+                sender.setTitle("Play", forState: .Normal)
+            }
         }
     }
     
