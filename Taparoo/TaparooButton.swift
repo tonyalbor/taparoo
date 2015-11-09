446c83b57853541fa96b98ade037aeb3b7f62e60
Tony Albor <tonyalbor92@gmail.com>
2015-11-09 14:09:18 -0800

    highlighted state for buttons

--- Taparoo/TaparooButton.swift
+++ Taparoo/TaparooButton.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
+// todo :: make the background color a bit brighter when it is the Tap It! button
+
 class TaparooButton: UIButton {
     
     var pointValue: Int = 1
     var number: UInt32 = 0
+    var taparooColor: UIColor?
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        taparooColor = backgroundColor
+        layer.cornerRadius = 2 // todo :: tweak
     }
     
     func reset() {
@@ -29,6 +34,20 @@ class TaparooButton: UIButton {
     func gameOver() {
         let title = number % 2 != 0 ? "Game" : "Over!"
         setTitle(title, forState: .Normal)
+    }
+    
+    override var highlighted: Bool {
+        get {
+            return super.highlighted
+        }
+        set {
+            if newValue {
+                backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.25)
+            } else {
+                backgroundColor = taparooColor
+            }
+            super.highlighted = newValue
+        }
     }
     
 }
