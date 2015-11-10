307170a3a22d0a269adaab4383bf231504bc9769
Tony Albor <tonyalbor92@gmail.com>
2015-11-09 19:31:43 -0800

    original colors

---
+++ UIColor+Taparoo.swift
@@ -0,0 +1,32 @@
+//
+//  UIColor+Taparoo.swift
+//  Taparoo
+//
+//  Created by Tony Albor on 11/9/15.
+//  Copyright © 2015 Tony Albor. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class func taparooRed() -> UIColor {
+        return UIColor(red: 220/255, green: 0/255, blue: 20/255, alpha: 1)
+    }
+    
+    class func taparooBlue() -> UIColor {
+        return UIColor(red: 0, green: 225/255, blue: 225/255, alpha: 1)
+    }
+    
+    class func taparooGreen() -> UIColor {
+        return UIColor(red: 0, green: 255/255, blue: 100/255, alpha: 1)
+    }
+    
+    class func taparooYellow() -> UIColor {
+        return UIColor(red: 255/255, green: 240/255, blue: 100/255, alpha: 1)
+    }
+    
+    class func taparooGreenBackground() -> UIColor {
+        return UIColor(red: 120/255, green: 190/255, blue: 155/255, alpha: 1)
+    }
+}
